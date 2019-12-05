8190fa37c8618585fe187e1089b6bad9a86b72a7
Inna-WD <inna.druker@gmail.com>
2019-12-05 15:20:15 -0500

    0512

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+// let calcButton = getElementById("calcBtn");
 
-function clerkInterface(){
-var number = prompt("Employee number:", "N1234567");
-var name=f
+function clerkInterface() {
+
+    var grossSalary;
+    let type = getElementById("type");
+    let code = getElementById("code");
+    let fixedSalary = getElementById("fixedSalary");
+    let hours = getElementById("hours");
+
+    if (type = r) {
+        if (hours == 160) {
+            grossSalary = fixedSalary;
+        } else if (hours < 160) {
+            grossSalary = hours * fixedSalary / 160;
+        } else {
+            grossSalary = fixedSalary + (hours - 160) * 2 * fixedSalary / 160;
+        }
+    }
+    else {
+    }
+    console.log(grossSalary)
 }
 
+// calcButton.addEventListener(`click`, clerkInterface);
 
 
 
@@ -25,16 +44,6 @@ var name=f
 
 
 
+// let dropDownList = document.getElementById("city");
 
-
-
-
-
-
-
-
-let dropDownList = document.getElementById("city");
-
-
-dropDownList.addEventListener("change",function(){
-}
+// dropDownList.addEventListener("change",function(){
