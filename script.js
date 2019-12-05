aa19bddbbbea75aa0cee1ceebc50c5067e6a3b30
Inna-WD <inna.druker@gmail.com>
2019-12-05 14:18:06 -0500

    0512

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function clerkInterface(){
+var number = prompt("Employee number:", "N1234567");
+var name=f
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let dropDownList = document.getElementById("city");
+
+
+dropDownList.addEventListener("change",function(){
+}
