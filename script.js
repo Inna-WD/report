e733f518926eadbcb394aafccef044936f7f3cce
Inna-WD <inna.druker@gmail.com>
2019-12-09 21:49:46 -0500

    0912

--- script.js
+++ script.js
@@ -2,20 +2,31 @@
 
 
 let calculate = document.getElementById("calculate");
-let type = document.getElementsByName("type");
-let code = document.getElementsByName("code");
-let fixedSalary = document.getElementsByName("fixedSalary");
-let hours = document.getElementsByName("hours");
-let fff=document.getElementsByName("subscribe");
-console.log(fff);
+let type = document.getElementsByName("type").value;
+let code = document.getElementsByName("code").value;
+let fixedSalary = document.getElementsByName("fixedSalary").value;
+let hours = document.getElementsByName("hours").value;
 
+
+console.log(type);
+console.log(hours);
 
 function clerkInterface() {
 
     var grossSalary = 0;
-    var txt;
-    if (type == "r") {
-        if (hours == 160) {
+    // var txt;
+    // var type = "r";
+    // for (var i = 0; i < typeElements.length; i++) {
+    //     if (typeElements[i].checked) {
+    //         type = type.Elements[i].value;
+    //         break;
+    //     }
+    // }
+
+
+
+    if (type === "r") {
+        if (hours === 160) {
             grossSalary = fixedSalary;
         } else if (hours < 160) {
             grossSalary = hours * fixedSalary / 160;
